1826f932b920f4ae560e9e8a71b9f7a3b31d35ed
xujin <xujin@bosonspace.com>
2022-06-22 16:35:55 +0800

    项目初始化，路由配置

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// 导入路由
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+
+// 配置路由规则
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Layout} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
