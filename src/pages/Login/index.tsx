428df7feb41df06abfe445c688802c4211eedf42
ferrytsui <ferrytsui@163.com>
2022-06-24 14:08:50 +0800

    feat：表单校验；获取表单数据

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,41 @@
 import { Button, NavBar, Form, Input } from 'antd-mobile'
 import styles from './index.module.scss'
 
+type LoginForm = {
+  mobie: string
+  code: string
+}
+
 const Login = () => {
+  const onFinish = (values: LoginForm) => {
+    console.log(values)
+  }
+
   return (
     <div className={styles.root}>
       <NavBar></NavBar>
       <div className="login-form">
         <h2 className="title">账号登录</h2>
 
-        <Form>
-          <Form.Item className="login-item">
+        <Form onFinish={onFinish} validateTrigger={['onBlur']}>
+          <Form.Item
+            className="login-item"
+            name="mobile"
+            validateTrigger="onBlur"
+            rules={[
+              { required: true, message: '请输入手机号' },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号格式错误'
+              }
+            ]}
+          >
             <Input placeholder="请输入手机号"></Input>
           </Form.Item>
           <Form.Item
+            name="code"
+            rules={[{ required: true, message: '请输入验证码' }]}
+            validateTrigger="onBlur"
             className="login-item"
             extra={<span className="code-extra">发送验证码</span>}
           >
